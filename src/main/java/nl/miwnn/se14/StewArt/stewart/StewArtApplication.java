103b735e24376e4422ba16bb854e15cc410a80ca
Luc Weerts <lucweerts@hotmail.com>
2024-11-11 16:10:47 +0100

    Placed Spring Boot files

---
+++ src/main/java/nl/miwnn/se14/StewArt/stewart/StewArtApplication.java
@@ -0,0 +1,13 @@
+package nl.miwnn.se14.StewArt.stewart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StewArtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StewArtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miwnn/se14/StewArt/stewart/StewArtApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwnn.se14.StewArt.stewart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StewArtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
