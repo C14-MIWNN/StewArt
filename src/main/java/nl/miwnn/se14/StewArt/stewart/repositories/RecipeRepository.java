ba4c053ebf019bb2f3f37188dd7fdf4502bbd518
Luc Weerts <lucweerts@hotmail.com>
2024-11-12 16:59:07 +0100

    WIP added fuzzy search for recipes

--- src/main/java/nl/miwnn/se14/StewArt/stewart/repositories/RecipeRepository.java
+++ src/main/java/nl/miwnn/se14/StewArt/stewart/repositories/RecipeRepository.java
@@ -4,5 +4,9 @@ package nl.miwnn.se14.StewArt.stewart.repositories;
 import nl.miwnn.se14.StewArt.stewart.model.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface RecipeRepository extends JpaRepository <Recipe, Long> {
+    Optional<List<Recipe>> findByTitleContaining(String searchTerm);
 }
